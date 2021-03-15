7c0efa2ce4b5b1dd5bb9ba536d4b97c7e8173d3c
Anastasia-pro <78647195+Anastasia-pro@users.noreply.github.com>
2021-03-15 22:26:11 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
-exports.min = function min (array) {
-  return 0;
+function max(arr) {
+    let max = arr[0];
+    for(let i=1; i<arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    if(arr.length === 0) {
+        return 0;
+    }
+    return max;
 }
 
-exports.max = function max (array) {
-  return 0;
+
+function min(arr) {
+    let min = arr[0];
+    for(let i=1; i<arr.length; i++) {
+        if(arr[i]< min){
+            min = arr[i];
+        }
+    }
+    if(arr.length === 0) {
+        return 0;
+    }
+    return min;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+function avg(arr) {
+    let sum  = 0;
+    for (let i = 0; i< arr[i]; i++) sum += arr[i];
+    return sum === 0 ? sum : sum / arr.length;
 }
